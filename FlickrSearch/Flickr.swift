d938ebb2f112a4ec6bd3c996bf16f484fcbf6f35
David Avendaño <d.avendano.c@hotmail.com>
2022-01-07 10:53:17 -0600

    Added the initial view for the flick search

---
+++ FlickrSearch/Flickr.swift
@@ -0,0 +1,12 @@
+//
+//  Flickr.swift
+//  FlickrSearch
+//
+//  Created by David Avendaño on 07/01/22.
+//
+
+import Foundation
+
+let apiKey = "616e2bb8100072e267fa2256e87a2597"
+
+
---
+++ FlickrSearch/FlickrPhotosCollectionVC.swift
@@ -0,0 +1,20 @@
+//
+//  FlickrPhotosCollectionViewController.swift
+//  FlickrSearch
+//
+//  Created by David Avendaño on 06/01/22.
+//
+
+import UIKit
+
+final class FlickrPhotosViewController: UICollectionViewController{
+    // MARK: - Properties
+    private let reuseIdentifier = "FlickrCell"
+    
+    private let sectionInsets =  UIEdgeInsets(
+        top: 50.0,
+        left: 20.0,
+        bottom: 50.0,
+        right: 20.0
+    )
+}
