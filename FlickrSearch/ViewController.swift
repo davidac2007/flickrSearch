6b68294ab7f6e526aa3fdada5f4efa91ae22907e
David Avendaño <d.avendano.c@hotmail.com>
2022-01-06 18:16:57 -0600

    Initial Commit

---
+++ FlickrSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrSearch
+//
+//  Created by David Avendaño on 06/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
